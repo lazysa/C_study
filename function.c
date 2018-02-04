9c7cb0ef729bedd8fe95b811baa6ff217e1e9753
Lazysa <linuxjosery@gmail.com>
2018-02-04 16:30:14 +0800

    新增函数的作用域演示

--- function.c
+++ function.c
@@ -4,6 +4,7 @@
 Test usage of function 
 */
 
+/*
 // 声明函数
 int increment(int a);
 
@@ -21,4 +22,33 @@ int main(int argc, char *argv[])
 
 int increment(int a) {
     return a + 1; 
+}
+
+*/
+
+// 演示函数的作用域
+// 声明交换函数
+void swap(int argc, char *argv[]);
+
+int main(int x, int y) 
+{
+    int x = 1;
+    int y = 2;
+
+    printf("x is %d\n", x);
+    printf("y is %d\n", y)
+    printf("Swapping...\n");
+    swap(x, y);
+    printf("Swapped!\n");
+    printf("x is %d\n", x);
+    printf("y is %d\n", y);
+}
+
+void swap(int a , int b) 
+{
+    int tmp = a;
+    a = b; 
+    b = tmp;
+    printf("a=%d\n", a);
+    printf("b=%d\n", b); 
 }
